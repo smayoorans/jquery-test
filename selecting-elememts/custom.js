bbeb8f84ec9229d0193ad1ca833cdcf32959dc7f
mayooran <mayooran@hsenidmobile.com>
2013-08-07 18:34:26 +0530

    Added two exercise

---
+++ selecting-elememts/custom.js
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mayooran
+ * Date: 8/7/13
+ * Time: 11:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(function() {
+    $('#selected-plays > li').addClass('horizontal');
+});
+
+
